4ede6b87bc156db0439bb4f8336149b8e336ce70
massenandev <massenandev@gmail.com>
2021-06-19 17:55:12 +0100

    feat(category): save category on database

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,11 +1,28 @@
 const express = require('express')
+const { route } = require('../articles/ArticlesController')
 const Category = require('./Category')
+const slugify = require('slugify')
 //permite que crie rotas sem a necessidade de usar a const app
 //o router é um obj utilizado pra criação de rotas
 const router = express.Router()
 
-router.get('/categories', (req, res) => {
-  res.send('rota')
+router.get('/admin/categories/new', (req, res) => {
+  res.render('admin/categories/new')
+})
+
+router.post('/categories/save', (req, res) => {
+  let title = req.body.title
+  if(title != undefined){
+    Category.create({
+      title,
+      //versao otimizada pra url
+      slug: slugify(title)
+    }).then(() => {
+      res.redirect('/')
+    })
+  } else {
+    res.redirect('/admin/categories/new')
+  }
 })
 
 //precisa exportar o router pra linkar o router com o arquivo principal (index)
