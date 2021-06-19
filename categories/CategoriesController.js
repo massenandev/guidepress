8aad90ef497f582fc46243eafe45933bee93ab4d
massenandev <massenandev@gmail.com>
2021-06-19 22:57:02 +0100

    feat(category): display all categories from db

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -26,7 +26,10 @@ router.post('/categories/save', (req, res) => {
 })
 
 router.get('/admin/categories', (req, res) => {
-  res.render('admin/categories/index')
+  Category.findAll().then(categories => {
+    res.render('admin/categories/index', { categories })
+
+  })
 })
 //precisa exportar o router pra linkar o router com o arquivo principal (index)
 module.exports = router
